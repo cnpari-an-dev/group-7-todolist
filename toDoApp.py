67f041a837d55b1bbf5630b5aab442be65234b91
robertgeraldsenasin <robertgeraldsenasin@gmail.com>
2025-10-07 14:50:04 +0800

    added Task class to represent tasks as objects

--- toDoApp.py
+++ toDoApp.py
@@ -1,21 +1,31 @@
 # toDoApp.py
 
-tasks=[]
+class Task:
 
-def addtask(task) :
-  tasks.append(task)
-  print("task added!")
+    def __init__(self, description: str):
+        self.description = description.strip()
 
-def showTasks( ):
-    if len(tasks)==0 :
-      print("no tasks yet")
+    def __str__(self):
+        return self.description
+
+
+def addtask(task):
+    tasks.append(task)
+    print("task added!")
+
+
+def showTasks():
+    if len(tasks) == 0:
+        print("no tasks yet")
     else:
-     for i in range (len(tasks)):
-      print(i+1,".",tasks[i])
+        for i in range(len(tasks)):
+            print(i+1, ".", tasks[i])
+
 
 def removetask(tasknumber):
-    tasks.pop(tasknumber) 
+    tasks.pop(tasknumber)
     print("task removed!!")
+
 
 def main():
     while True:
@@ -24,16 +34,18 @@ def main():
         print("3.Remove Task")
         print("4- Exit")
         ch = input("enter choice : ")
-        if ch=="1":
+        if ch == "1":
             t = input("enter task : ")
             addtask(t)
-        elif ch=="2":
+        elif ch == "2":
             showTasks()
-        elif ch=="3":
-            n=int(input("enter task no to remove: "))
-            removetask(n)   
-        elif ch=="4":
-            break;
+        elif ch == "3":
+            n = int(input("enter task no to remove: "))
+            removetask(n)
+        elif ch == "4":
+            break
         else:
             print("wrong choice!!")
+
+
 main()
